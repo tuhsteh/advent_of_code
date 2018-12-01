610fb6b2e7f310380954a31c360efecc8f583336
tommy tom <t.stear@gmail.com>
2018-12-01 14:35:15 -0700

    I found my problem, sort of.  at least, now this is working.  and it takes 140 times through all the results to find a repeat.

--- 02/changes.py
+++ 02/changes.py
@@ -1,76 +1,46 @@
 #!/usr/bin/env python3
-#
-##Day 1, advent of code
-##  find the first repeated "Frequency" that is calculated,
-##  given a list of changes to apply.
-#
-#DEBUG = True
-#
-#def __log__(msg):
-#    if DEBUG:
-#        print(msg)
-#
-#def get_changes(filename = 'input.txt'):
-#    f = open(filename, 'r')
-#    changes = []
-#    try:
-#        for line in f.readlines():
-##            __log__(int(line))
-#            changes.append(int(line))
-#    except Exception as e:
-#        __log__(e)
-#    return changes
-#
-#
-#
-#if __name__ == "__main__":
-#    __log__('Getting list of inputs...')
-##    changes = get_changes('sample1.txt')
-#    changes = get_changes()
-#    __log__('Done.')
-#    result = 0
-#    __log__('Applying inputs...')
-#    seen = set()
-#    seen.add(0)
-#    for d in changes:
-#        result = result + d
-#        __log__('%8d %12d' % (d,result))
-#        if result in seen:
-#            print('Found a repeated frequency:  %d' % result)
-#            break            
-#        seen.add(result)
-#        __log__(seen)
-##    result = 0
-#    for d in changes:
-#        result = result + d
-#        __log__('%8d %12d' % (d,result))
-#        if result in seen:
-#            print('Found a repeated frequency:  %d' % result)
-#            break            
-#        seen.add(result)
-#    for d in changes:
-#        result = result + d
-#        __log__('%8d %12d' % (d,result))
-#        if result in seen:
-#            print('Found a repeated frequency:  %d' % result)
-#            break            
-#        seen.add(result)
-#    for d in changes:
-#        result = result + d
-#        __log__('%8d %12d' % (d,result))
-#        if result in seen:
-#            print('Found a repeated frequency:  %d' % result)
-#            break            
-#        seen.add(result)
-#    for d in changes:
-#        result = result + d
-#        __log__('%8d %12d' % (d,result))
-#        if result in seen:
-#            print('Found a repeated frequency:  %d' % result)
-#            break            
-#        seen.add(result)
-#    print('Done.  Found %d' % result)
 
+#Day 1, advent of code
+#  find the first repeated "Frequency" that is calculated,
+#  given a list of changes to apply.
+
+DEBUG = False
+
+def __log__(msg):
+    if DEBUG:
+        print(msg)
+
+def get_changes(filename = 'input.txt'):
+    return [int(c) for c in open(filename,'r').readlines()]
+
+def look_for_dupe(changes):
+    result = 0
+    loop_count = 0
+    seen = set([0])
+    while True:
+        loop_count += 1
+        for d in changes:
+            result = result + d
+            __log__('%8d %12d' % (d,result))
+            if result in seen:
+                print('Found a repeated frequency after %d loops:  %d' % (loop_count, result))
+                return           
+            seen.add(result)
+            __log__(seen)
+
+
+
+
+
+if __name__ == "__main__":
+    __log__('Getting list of inputs...')
+#    changes = get_changes('sample1.txt')
+    changes = get_changes()
+    __log__('Done.')
+    __log__('Applying inputs...')
+    look_for_dupe(changes)
+
+            
 import itertools
 data = [int(x) for x in open('input.txt','r').readlines()]
 print(sum(data))
