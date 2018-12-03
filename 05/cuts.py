3fd6c691821028c5984148e4d0ca85029b2f5f33
tommy tom <t.stear@gmail.com>
2018-12-03 08:11:18 -0700

    I have no idea what i'm doing here

---
+++ 05/cuts.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from util import get_input
+import re
+from collections import defaultdict
+
+
+data = get_input()
+claims = []
+for d in data:
+#    claim_id, start_x, start_y, width, height
+    numbers = list(map(int, re.findall(r'-?\d+', d)))
+    claims.append(numbers)
+
+material = defaultdict(list)
+overlaps = {}
+for (claim_id, start_x, start_y, width, height) in claims:
+    overlaps[claim_id] = set()
+    for i in range(start_x, start_x + width):
+        for j in range(start_y, start_y + height):
+            if material[(i+j)]:
+                for number in material[(i+j)]:
+                    overlaps[number].add(claim_id)
+                    overlaps[claim_id].add(number)
+            material[(i+j)].append(claim_id)
+
+print("a", len([cut for cut in material if len(material[cut]) > 1]))
+print("b", [k for k in overlaps if len(overlaps[k]) == 0])
+    
+    
+    
+    
---
+++ 05/util.py
@@ -0,0 +1,8 @@
+
+
+
+
+def get_input(filename = 'input.txt'):
+    return [c for c in open(filename,'r').readlines()]
+
+
