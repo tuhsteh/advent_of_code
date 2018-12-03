3ed4f9acbced37e8b1512365ced6b37de59c2490
tommy tom <t.stear@gmail.com>
2018-12-03 08:32:07 -0700

    Ok.  if we treat the material as a grid, then for each square inch in the grid, we can add the claim_id for any claims that include that square.  then, after all the claims are covered, each square inch of the material will have a list of any associated claim ids.

--- 05/cuts.py
+++ 05/cuts.py
@@ -9,19 +9,13 @@ data = get_input()
 claims = []
 for d in data:
 #    claim_id, start_x, start_y, width, height
-    numbers = list(map(int, re.findall(r'\d+', d)))
-    claims.append(numbers)
+    claims.append(list(map(int, re.findall(r'\d+', d))))
 
+    
 material = defaultdict(list)
-overlaps = {}
 for (claim_id, start_x, start_y, width, height) in claims:
-    overlaps[claim_id] = set()
     for i in range(start_x, start_x + width):
         for j in range(start_y, start_y + height):
-            if material[(i,j)]:
-                for number in material[(i,j)]:
-                    overlaps[number].add(claim_id)
-                    overlaps[claim_id].add(number)
             material[(i,j)].append(claim_id)
 
 print("square inches of overlap:  %d" % len([cut for cut in material if len(material[cut]) > 1]))
