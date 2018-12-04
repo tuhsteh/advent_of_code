000975d2c368f0911c0ddb929bcd36a599fddb60
tommy stear <tom.stear@vitalize.com>
2018-12-04 10:43:40 -0700

    I need to have more faith in the order of my lines coming in.

--- 07/guard.py
+++ 07/guard.py
@@ -1,67 +1,76 @@
 #!/usr/bin/env python
 from util import get_input
 from collections import defaultdict
+import dateparser
 import re
 
-data = get_input(filename = 'sorted_input.txt')
+data = get_input(filename='input.txt')
 data.sort()
 guards = defaultdict(list)
+times = defaultdict(int)
 
 
-def parse_log(log):
-    date = log.split(' ')[0].strip('[')
-    time = log.split(' ')[1].strip(']')
-    msg = ' '.join(log.split(' ')[2:])
-    time = 60 * (int(time.split(':')[0])) + (int(time.split(':')[1]))
-    return date, time, msg
+# def parse_log(log):
+#     date = log.split(' ')[0].strip('[')
+#     time = log.split(' ')[1].strip(']')
+#     msg = ' '.join(log.split(' ')[2:])
+#     time = 60 * (int(time.split(':')[0])) + (int(time.split(':')[1]))
+#     return date, time, msg
+#
+#
+# def get_sleep_minutes(i, data, guards):
+#     start = None
+#     end = None
+#     while True:
+#         i += 1
+#         try:
+#             date, time, msg = parse_log(data[i])
+#         except Exception as e:
+#             print("after %d loops, %s" % (i,e))
+#             return i
+#         if -1 < msg.find('Guard'):
+#             return i
+#         if -1 < msg.find('asleep'):
+#             start = time
+#         if -1 < msg.find('wakes'):
+#             end = time
+#         if start is not None and end is not None:
+#             guards[guard_id].extend([t for t in range(start,end)])
+#             start = None
+#             end = None
 
+guard_id = None
+start = None
+end = None
 
-def get_sleep_minutes(i, data, guards):
-    start = None
-    end = None
-    while True:
-        i += 1
-        try:
-            date, time, msg = parse_log(data[i])
-        except Exception as e:
-            print("after %d loops, %s" % (i,e))        
-            return i
-        if -1 < msg.find('Guard'):
-            return i
-        if -1 < msg.find('asleep'):
-            start = time
-        if -1 < msg.find('wakes'):
-            end = time
-        if start is not None and end is not None:
-            guards[guard_id].extend([t for t in range(start,end)])
-            start = None
-            end = None
+# import ipdb
+# ipdb.set_trace()
+
+for line in data:
+    time, action = line.split('] ')
+
+    time = dateparser.parse(time[1:])
     
+    if action.startswith('Guard'):
+        guard_id = int(re.findall(r'\#(\d+)', action)[0])
+    if action.startswith('falls'):
+        start = time
+    if action.startswith('wakes'):
+        end = time
+        guards[guard_id].append((start.minute, end.minute))
+        times[guard_id] += (end - start).seconds
 
+# import ipdb
+# ipdb.set_trace()
 
-for i in range(len(data)):
-    date, time, msg = parse_log(data[i])
-    
-#    import ipdb
-#    ipdb.set_trace()
-    
-    if -1 < msg.find('Guard'):
-        guard_id = re.findall(r'\#(\d+)', msg)[0]
-        guards[guard_id] = list()
-        i = get_sleep_minutes(i, data, guards)
+(guard, time) = max(times.items(), key=lambda i: i[1])
+(minute, count) = max([
+    (minute, sum(1 for start, end in guards[guard] if start <= minute < end))
+    for minute in range(60)], key=lambda i: i[1])
 
-max = -1
-sleepy_guard = None
-map(print, guards)
-for g in guards:
-    total_minutes = len(guards[g])
-    if total_minutes > max:
-        sleepy_guard = g
-        max = total_minutes
-
-print(sleepy_guard)
-print(max)
-print(int(max) * int(sleepy_guard))
+print(guard)
+print(minute)
+print(guard * minute)
 
     
         
