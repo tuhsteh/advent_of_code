3311d1d46fd662d22f6cd155534dea2926ee0e74
tommy tom <t.stear@gmail.com>
2018-12-04 23:32:46 -0700

    I got it to work, by defining the 'i' index outside the loop first.  sometimes scope gets the better of me.

--- 09/polymer.py
+++ 09/polymer.py
@@ -2,34 +2,32 @@
 from util import get_input
 
 
-chain = get_input(filename = 'sample.txt')[0]
-print(chain)
+#chain = get_input(filename = 'sample.txt')[0]
+chain = get_input()[0]
+#print(chain)
 
-import ipdb
-ipdb.set_trace()
-
+#import ipdb
+#ipdb.set_trace()
+i = None
 while True:
-    i = 0
-    changes = 0
+    changes = None
     for i in range(0, len(chain)-1):
         print(i)
         changes = 0
         try:
             char1,char2 = chain[i],chain[i+1]
-            print('%s%s' % (char1,char2))
+#            print('%s%s' % (char1,char2))
             if char1.lower() == char2.lower():
-                print('chars are same letter:  %s %s' % (char1,char2))
+#                print('chars are same letter:  %s %s' % (char1,char2))
                 if char1 is not char2:
-                    print('removing %s and %s' % (char1,char2))
+#                    print('removing %s and %s' % (char1,char2))
                     chain = chain[:i] + chain[i+2:]
-                    print('chain:  %s' % chain)
+#                    print('chain:  %s' % chain)
                     changes = 1
         except:
             pass
-            i = 0
-    i = 0
     if changes == 0:
         break
 
-print(chain)
+print(len(chain))
 
