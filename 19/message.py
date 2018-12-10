628465404f5dc6c4d73db7b6c55296a3a93f4673
tommy tom <t.stear@gmail.com>
2018-12-09 23:43:43 -0700

    This solution actually watches for small bounding boxes, so it doesn't print EVERY night sky.  there are an infinite number of them.

--- 19/message.py
+++ 19/message.py
@@ -1,60 +1,31 @@
 #!/usr/bin/env python
 
 from util import get_input
-from collections import defaultdict
 import re
-import operator
 
 #data = get_input(filename = 'sample.txt')
 data = get_input()
 points = []
 for d in data:
-    x,y,dx,dy = list(map(int, re.findall(r'-?\d+',d)))
-    points.append((x,y,dx,dy))
+    x,y,vx,vy = list(map(int, re.findall(r'-?\d+',d)))
+    points.append([x,y,vx,vy])
     
-max_x = max([p[0] for p in points])
-max_y = max([p[1] for p in points])
-min_x = min([p[0] for p in points])
-min_y = min([p[1] for p in points])
+for t in range(100000):
+    min_x = min([x for x,y,_,_ in points])
+    max_x = max([x for x,y,_,_ in points])
+    min_y = min([y for x,y,_,_ in points])
+    max_y = max([y for x,y,_,_ in points])
+    W = 100
+    if min_x+W >= max_x and min_y + W >= max_y:
+        print(t,min_x, max_x, min_y, max_y)
+        for y in range(min_y, max_y+1):
+            for x in range(min_x, max_x+1):
+                if (x,y) in [(px,py) for px,py,_,_ in points]:
+                    print('#', end=''),
+                else:
+                    print('.', end=''),
+            print()
 
-
-print(points)
-
-print('%d, %d' % (max_x, max_y))
-print('%d, %d' % (min_x, min_y))
-
-def init_sky():
-    sky = []
-    for x in range(0,max_x+3*abs(min_x)):
-        row = []
-        for y in range(0,max_y+3*abs(min_y)):
-            row.append('.')
-        sky.append(row)
-    for row in sky:
-        print(''.join(row))
-    return sky
-
-def print_sky(points, iteration):
-    sky = init_sky()
-    x,y,dx,dy = None,None,None,None
-    try:
-        for p in points:
-            x,y,dx,dy = p
-            x += abs(min_x)
-            y += abs(min_y)
-            px,py = list(map(operator.mul, [dx,dy], [iteration,iteration]))
-            x += px
-            y += py
-            sky[y][x] = '#'
-    except IndexError as e:
-        print('%d, %d, %d, %d' % (x,y,dx,dy))
-    for row in sky:
-        print(''.join(row))
-
-
-#import ipdb
-#ipdb.set_trace()
-        
-for i in range(10):
-    print('-'*40)
-    print_sky(points,i)
+    for p in points:
+        p[0] += p[2]
+        p[1] += p[3]
