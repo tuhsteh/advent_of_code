1370640d96a6d2b386d098536bdcac792fa8632c
tommy tom <t.stear@gmail.com>
2018-12-11 23:18:15 -0700

    this solution works, and it's so cool:  use numpy, with its sort of a map function, and prepopulate the grid one time.

--- 21/battery.py
+++ 21/battery.py
@@ -1,38 +1,22 @@
 #!/usr/bin/env python
-
+import numpy
 
 serial = 5153
 
 
-def power(x, y, serial):
+def power(x, y):
     rack_id = x + 10
-    increase = rack_id * y
+    begin = rack_id * y
+    increase = begin + serial
     set_level = increase * rack_id
     keep = set_level // 100 % 10
     return (keep - 5)
 
-def add_cells(cells):
-    sum = 0
-    for i in range(len(cells)):
-        for j in range(len(cells[i])):
-            sum+= cells[i][j]
-    return sum
+grid = numpy.fromfunction(power, (300, 300))
 
-max_power = -1000000
-max_x, max_y = 0,0
-for x in range(1, 300-3):
-    for y in range(1, 300-3):
-        cells = [[0,0,0],[0,0,0],[0,0,0]]
-        for a in range(3):
-            for b in range(3):
-                cells[a][b] = power(x+a,y+b,serial)
-#
-#        import ipdb
-#        ipdb.set_trace()
 
-        total = add_cells(cells)
-        if total > max_power:
-            max_x,max_y = x,y
-            max_power = total
-
-print('max_power %d, x %d, y %d' % (max_power,max_x,max_y))
+width = 3
+windows = sum(grid[x:x-width+1 or None, y:y-width+1 or None] for x in range(width) for y in range(width))
+maximum = int(windows.max())
+location = numpy.where(windows == maximum)
+print(width, maximum, location[0][0], location[1][0])
