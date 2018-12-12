ded4f18335787bd34873224779936c8af489e609
tommy tom <t.stear@gmail.com>
2018-12-11 23:19:04 -0700

    Not terribly elegant, but i could keep track of a maximum power after going through the whole thing, and then only print the one i care about.

---
+++ 22/battery.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import numpy
+
+serial = 5153
+
+
+def power(x, y):
+    rack_id = x + 10
+    begin = rack_id * y
+    increase = begin + serial
+    set_level = increase * rack_id
+    keep = set_level // 100 % 10
+    return (keep - 5)
+
+grid = numpy.fromfunction(power, (300, 300))
+
+for width in range(3, 300):
+    windows = sum(grid[x:x-width+1 or None, y:y-width+1 or None] for x in range(width) for y in range(width))
+    maximum = int(windows.max())
+    location = numpy.where(windows == maximum)
+    print(width, maximum, location[0][0], location[1][0])
