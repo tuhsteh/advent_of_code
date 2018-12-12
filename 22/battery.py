ef8fb8b2b9c5a6392f663297b7d58e348aca7b53
tommy tom <t.stear@gmail.com>
2018-12-11 23:26:09 -0700

    Something like this.  yeah

--- 22/battery.py
+++ 22/battery.py
@@ -14,8 +14,19 @@ def power(x, y):
 
 grid = numpy.fromfunction(power, (300, 300))
 
-for width in range(3, 300):
-    windows = sum(grid[x:x-width+1 or None, y:y-width+1 or None] for x in range(width) for y in range(width))
-    maximum = int(windows.max())
-    location = numpy.where(windows == maximum)
-    print(width, maximum, location[0][0], location[1][0])
+biggest_max = -1000
+biggest_location = None
+biggest_width = None
+try:
+    for width in range(3, 300):
+        if width % 10 == 0:
+            print('checking box size of %d' % width)
+        windows = sum(grid[x:x-width+1 or None, y:y-width+1 or None] for x in range(width) for y in range(width))
+        maximum = int(windows.max())
+        location = numpy.where(windows == maximum)
+        if maximum > biggest_max:
+            biggest_max = maximum
+            biggest_location = location
+            biggest_width = width
+except KeyboardInterrupt:
+     print(biggest_width, biggest_max, biggest_location[0][0], biggest_location[1][0])
